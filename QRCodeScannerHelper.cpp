10da0da8b26ddadea0cb937ef443f023787312b8
Bugrahan <donmezbugrahan@gmail.com>
2020-01-03 22:39:20 +0300

    Add angle

--- QRCodeScannerHelper.cpp
+++ QRCodeScannerHelper.cpp
@@ -11,6 +11,15 @@
 using namespace cv;
 using namespace std;
 
+double angle(cv::Point pt1, cv::Point pt2, cv::Point pt0) {
+
+    double dx1 = pt1.x - pt0.x;
+    double dy1 = pt1.y - pt0.y;
+    double dx2 = pt2.x - pt0.x;
+    double dy2 = pt2.y - pt0.y;
+    return (dx1 * dx2 + dy1 * dy2) / sqrt((dx1 * dx1 + dy1 * dy1) * (dx2 * dx2 + dy2 * dy2) + 1e-10);
+}
+
 void displayQRCode(Mat &im, Mat &bbox) {
 
     int n = bbox.rows;
