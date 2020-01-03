f7349cfd49aee19a95ef791300e5e55c1fff88be
Bugrahan <donmezbugrahan@gmail.com>
2020-01-03 22:34:51 +0300

    Add display function for QR Code

--- QRCodeScannerHelper.cpp
+++ QRCodeScannerHelper.cpp
@@ -2,4 +2,22 @@
 // Created by Buğrahan Dönmez on 3.01.2020.
 //
 
-#include "QRCodeScannerHelper.h"
+#include <opencv2/objdetect.hpp>
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <iostream>
+
+using namespace cv;
+using namespace std;
+
+void displayQRCode(Mat &im, Mat &bbox) {
+
+    int n = bbox.rows;
+    for (int i = 0; i < n; i++) {
+
+        line(im, Point2i(bbox.at<float>(i, 0), bbox.at<float>(i, 1)),
+             Point2i(bbox.at<float>((i + 1) % n, 0), bbox.at<float>((i + 1) % n, 1)), Scalar(255, 0, 0), 3);
+    }
+    imshow("Result", im);
+}
