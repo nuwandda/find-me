94db4d0cc28a301d983e93ebdb2ba532217c7477
Bugrahan <donmezbugrahan@gmail.com>
2020-01-03 22:28:45 +0300

    Create QR Code Scanner files

---
+++ QRCodeScannerHelper.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Buğrahan Dönmez on 3.01.2020.
+//
+
+#include "QRCodeScannerHelper.h"
---
+++ QRCodeScannerHelper.h
@@ -0,0 +1,14 @@
+//
+// Created by Buğrahan Dönmez on 3.01.2020.
+//
+
+#ifndef NC_RD_FACEMASK_QRCODESCANNERHELPER_H
+#define NC_RD_FACEMASK_QRCODESCANNERHELPER_H
+
+
+class QRCodeScannerHelper {
+
+};
+
+
+#endif //NC_RD_FACEMASK_QRCODESCANNERHELPER_H
