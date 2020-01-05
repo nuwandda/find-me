e216586ede8bb53f37a0b291bc27c62c7ece6cbc
Bugrahan <donmezbugrahan@gmail.com>
2020-01-05 18:04:28 +0300

    Add point validation

--- QRCodeScannerHelper.cpp
+++ QRCodeScannerHelper.cpp
@@ -36,7 +36,20 @@ double calculateAreaOfPolygon(vector<Point> points) {
 
 void cropQRCode(vector<Point> points, Mat &cropped, const Mat &image) {
 
-    Rect roi(points[3].x, points[3].y, points[1].x - points[3].x, points[1].y - points[3].y);
+    int sum = 0;
+    Point point;
+    for (int i = 0; i < points.size(); i++) {
+        if (i == 0) {
+            sum = (points[i].x + points[i].y);
+            point = points[i];
+        }
+
+        if ((points[i].x + points[i].y) < sum) {
+            sum = (points[i].x + points[i].y);
+            point = points[i];
+        }
+    }
+    Rect roi(point.x, point.y, abs(points[2].x - points[0].x), abs(points[2].y - points[0].y));
     cropped = image(roi);
 }
 
